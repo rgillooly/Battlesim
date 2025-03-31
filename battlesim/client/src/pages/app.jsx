ed511884172902fb8e72f6f5843aac91778257d9
Ryan Gillooly <rpgillooly@gmail.com>
2025-03-31 19:13:01 -0400

    corrected spelling mistake

--- battlesim/client/src/pages/app.jsx
+++ battlesim/client/src/pages/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./home";
+import BattleSimulator from "./BattleSimulator";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Submission from "./submission";
